248d81ff674e6cc27780044351e77cceddffa8a9
Alberto Castro <allber.analista@gmail.com>
2022-02-16 17:16:25 -0300

    FormularioSQLcrud.php
    FormularioSQLcrud.php

---
+++ formularioSQL.php/conexao.php
@@ -0,0 +1,20 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$dbname = "usuarios";
+
+//Criar a conexao
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+
+
+/* REVISADO ACIMA
+
+$servidor = "localhost";
+$usuarios = "root";
+$senha = "";
+$dbname = "usuarios";
+
+$conn = mysqli_connect( $servidor, $usuarios, $senha, $dbname);
+
+?> */
